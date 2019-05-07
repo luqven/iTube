4d4b5c67d2a9f3496550a1c13acb84223722bab0
Luis H. Bajj Jr <luqven@gmail.com>
2019-05-07 18:03:08 -0400

    fix hamburger menu styling

--- frontend/components/nav_bar/hamburger_menu/hamburger.jsx
+++ frontend/components/nav_bar/hamburger_menu/hamburger.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-export const HamburgerMenu = (props) => {
-  const menu = document.querySelector(".hamburger-ul")
-  const outSideMenu = document.querySelector(".hamburger-bkg")
-  return(
+export const HamburgerMenu = props => {
+  const menu = document.querySelector(".hamburger-ul");
+  const outSideMenu = document.querySelector(".hamburger-bkg");
+  return (
     <>
-      <section onClick={() => { menu.classList.toggle('hidden'); outSideMenu.classList.toggle('hidden') }}
-        className="hamburger-bkg hidden">
-      </section>
-      <p onClick={() => { menu.classList.toggle('hidden'); outSideMenu.classList.toggle('hidden')}} 
-        className="hamburger-btn">
+      <section
+        onClick={() => {
+          menu.classList.toggle("slide");
+          outSideMenu.classList.toggle("hidden");
+        }}
+        className="hamburger-bkg hidden"
+      />
+      <p
+        onClick={() => {
+          menu.classList.toggle("slide");
+          outSideMenu.classList.toggle("hidden");
+        }}
+        className="hamburger-btn"
+      >
         <FontAwesomeIcon icon={["fas", "bars"]} />
       </p>
-      <ul className="hamburger-ul hidden">
-        <li><a href="https://github.com/luqven/iTube">Github <FontAwesomeIcon icon={["fab", "github"]} /></a></li>
-        <li><a href="https://www.linkedin.com/in/luisballjr/">Linkedin <FontAwesomeIcon icon={["fab", "linkedin"]} /></a></li>
-        <li><a href="https://angel.co/luis-h-ball">AngelList <FontAwesomeIcon icon={["fab", "angellist"]} /></a></li>
+      <ul className="hamburger-ul slide">
+        <li>
+          <a href="https://github.com/luqven/iTube">
+            Github <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/luisballjr/">
+            Linkedin <FontAwesomeIcon icon={["fab", "linkedin"]} />
+          </a>
+        </li>
+        <li>
+          <a href="https://angel.co/luis-h-ball">
+            AngelList <FontAwesomeIcon icon={["fab", "angellist"]} />
+          </a>
+        </li>
       </ul>
     </>
-  )
-}
+  );
+};
